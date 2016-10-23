d189a522b5d3537a6169da2ddfb8cb9ed1edf948
Robert Corrigan <rjcorrig@gmail.com>
2016-10-23 17:41:10 -0400

    Add unit testing framework

---
+++ INI.framework Tests/INI_framework_Tests.m
@@ -0,0 +1,39 @@
+//
+//  INI_framework_Tests.m
+//  INI.framework Tests
+//
+//  Created by Robert Corrigan on 10/23/16.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface INI_framework_Tests : XCTestCase
+
+@end
+
+@implementation INI_framework_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end

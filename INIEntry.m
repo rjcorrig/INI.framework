4f3aa07939d444c72ddf52521666bffaa55d7137
Robert Corrigan <rjcorrig@gmail.com>
2016-11-02 23:00:30 -0400

    Reimplement [INIEntry parse] with Regular Expressions

--- INIEntry.m
+++ INIEntry.m
@@ -41,126 +41,62 @@
   info.key = NSMakeRange(0, 0);
   info.value = NSMakeRange(0, 0);
   type = INIEntryTypeOther;
-  
-  if ([[line stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length]) {
-    NSUInteger n = line.length;
-    NSUInteger j;
-    char state = ' ';
-    for (NSUInteger i = 0; i < n; i++) {
-      switch (state) {
-          
-        // Whitespace at the beginning of line or initial state
-        case ' ': 
-          switch ([line characterAtIndex: i]) {
-              
-            case ' ':
-            case '\t':
-              break;
-              
-            case '[':
-              state = '[';
-              info.section.location = i + 1;
-              type = INIEntryTypeSection;
-              break;
-            
-            case '#':
-            case ';':
-              state = '.';
-              type = INIEntryTypeComment;
-              break;
-              
-            default:
-              state = 'k';
-              info.key.location = i;
-              type = INIEntryTypeKeyValue;
-              break;
-          }
-          break;
-          
-        // Section
-        case '[':
-          switch ([line characterAtIndex: i]) {
-            case ']':
-              info.section.length = i - info.section.location;
-              state = '.';
-              break;
-          }
-          break;
-          
-        // Key
-        case 'k':
-          switch ([line characterAtIndex: i]) {
-            case ' ':
-              // let's look ahead to see if it's the end of the key or just a key with a space in the middle
-              for (j = i; j < n && ([line characterAtIndex: j] == ' ' || [line characterAtIndex: j] == '\t'); j++) {}
-              if (j == n) {
-                // malformed entry, we were looking for = sign after the key and reached end of the line
-                state = '.';
-		info.key.location = 0;
-                type = INIEntryTypeOther;
-              } else {
-                if ([line characterAtIndex: j] == '=') {
-                  // the key finished at i - 1 (before this whitespace), there were just whitespaces before = sign
-                  info.key.length = i - info.key.location;
-                  // ...let's skip all those whitespaces, equal sign and all whitespaces after the sign
-                  // and jump straight to 'v'alue state
-                  for (j = j + 1; j < n && ([line characterAtIndex: j] == ' ' || [line characterAtIndex: j] == '\t'); j++) {}
+	
+	NSError *error;
+	NSRegularExpression *regex;
+	NSRange lineRange = NSMakeRange(0, [self.line length]);
 
-                  if (j == n) {
-                    // value is blank
-                    info.value.length = 0;
-                    state = '.';
-                  } else {
-                    state = 'v';
-                  }
+	// Key-value pair
+	regex = [NSRegularExpression
+		 regularExpressionWithPattern:@"^\\s*([^=;#\\r\\n]+?)\\s*=\\s*([^;#\\r\\n]*)\\s*"
+		 options:0
+		 error:&error];
+	
+	if (regex) {
+		NSTextCheckingResult *match = [regex firstMatchInString:self.line options:0 range:lineRange];
+		if (match) {
+			info.key = [match rangeAtIndex:1];
+			info.value = [match rangeAtIndex:2];
+			
+			// Trim info.value.length to remove trailing spaces
+			NSString *valueTrim = [self.line substringWithRange:info.value];
+			valueTrim = [valueTrim stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+			info.value.length = [valueTrim length];
+			
+			type = INIEntryTypeKeyValue;
+			return;
+		}
+	}
 
-                  i = j - 1;
-                  info.value.location = j;
-
-		} else {
-                  // we're still in the key with the whitespaces in the middle
-                  // let's skip them and continue inside 'k'ey state
-                  i = j - 1;
-                }
-              }
-              break;
-          }
-          break;
-          
-        // Value
-        case 'v':
-          if (i == n - 1) {
-            // it's the last char
-            info.value.length = (i + 1) - info.value.location;
-            state = '.';
-          } else {
-            // it's not the last char
-            switch ([line characterAtIndex: i]) {
-              case ' ':
-                // are those white spaces to ignore just before the end of line?
-                for (j = i; j < n && ([line characterAtIndex: j] == ' ' || [line characterAtIndex: j] == '\t'); j++) {}
-                if (j == n) {
-                  // ...yep, we've got the value - capture it without the whitespaces at the end and go to '.'final state
-                  info.value.length = i - info.value.location;
-                  state = '.';
-                }
-                break;
-            }
-          }
-          break;
-        
-        // Fin
-        case '.':
-          i = n;
-          break;
-        
-      }
-    }
-    
-    if (state != '.') {
-      type = INIEntryTypeOther;
-    }
-  }
+	// Section
+	regex = [NSRegularExpression
+		 regularExpressionWithPattern:@"^\\s*\\[([^\\]\\r\\n]+)]\\s*"
+		 options:0
+		 error:&error];
+	
+	if (regex) {
+		NSTextCheckingResult *match = [regex firstMatchInString:self.line options:0 range:lineRange];
+		if (match) {
+			info.section = [match rangeAtIndex:1];
+			type = INIEntryTypeSection;
+			return;
+		}
+	}
+	
+	// Comment
+	regex = [NSRegularExpression
+		 regularExpressionWithPattern:@"^\\s*[;#]+"
+		 options:0
+		 error:&error];
+	
+	if (regex) {
+		NSTextCheckingResult *match = [regex firstMatchInString:self.line options:0 range:lineRange];
+		if (match) {
+			type = INIEntryTypeComment;
+			return;
+		}
+	}
+	
 }
 
 - (NSString *) key {

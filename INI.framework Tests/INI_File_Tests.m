6dc78dbd921a5639cea20cc774ab3c1f723e799e
Robert Corrigan <rjcorrig@gmail.com>
2016-11-04 21:53:46 -0400

    Regenerate a writeable INI file before each test

--- INI.framework Tests/INI_File_Tests.m
+++ INI.framework Tests/INI_File_Tests.m
@@ -16,13 +16,22 @@
 @implementation INI_File_Tests
 
 - (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+	[super setUp];
+	
+	NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+	NSURL *source = [bundle URLForResource:@"test_lf" withExtension:@"ini"];
+	NSURL *dest = [[source URLByDeletingLastPathComponent] URLByAppendingPathComponent:@"test_write.ini"];
+	NSFileManager *fm = [[NSFileManager alloc] init];
+	
+	NSError *err;
+	[fm removeItemAtURL:dest error:&err];
+	[fm copyItemAtURL:source toURL:dest error:&err];
+	XCTAssertNil(err);
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
+	// Put teardown code here. This method is called after the invocation of each test method in the class.
+	[super tearDown];
 }
 
 - (void)testINIFile_initWithUTF8ContentsOfFile_CRLF {
